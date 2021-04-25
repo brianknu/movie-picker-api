1f43fc11ba7b56b7dc0553cb7d1aaf86856ffffd
Brian <brianknussell@gmail.com>
2021-04-25 17:23:36 -0300

    Now the gets return an id

--- movies/entities/recommendation.py
+++ movies/entities/recommendation.py
@@ -1,4 +1,5 @@
 class Recommendation:
-    def __init__(self, movie, user):
+    def __init__(self, movie, user, mongo_id=None):
         self.movie = movie
         self.user = user
+        self.mongo_id = mongo_id
--- movies/repositories/recommendations_repository.py
+++ movies/repositories/recommendations_repository.py
@@ -7,18 +7,20 @@ def get_all_movies(db_cl):
     all_recommendations_from_db = db_cl.find({})
     r_list = []
     for item in all_recommendations_from_db:
+        movie_id = str(item['_id'])
         movie = Movie(item['trailer'], item['description'])
         user = User(item['user'])
-        r_list.append(Recommendation(movie, user))
+        r_list.append(Recommendation(movie, user, movie_id))
     return r_list
 
 
 def get_random_movie(db_cl):
     random_movie_cursor = db_cl.aggregate([{'$sample': {'size': 1}}])
     random_movie = next(random_movie_cursor)
+    random_movie_id = str(random_movie['_id'])
     movie = Movie(random_movie['trailer'], random_movie['description'])
     user = User(random_movie['user'])
-    return Recommendation(movie, user)
+    return Recommendation(movie, user, random_movie_id)
 
 
 def insert_recommendation(db_cl, request_data):
